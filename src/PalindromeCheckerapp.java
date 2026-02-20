04c7dc6ac8475fcd02590fc1af2f707ab55cc8cd
Aishwarya Batra <aishwaryabatra10@gmail.com>
2026-02-20 09:16:52 +0530

    [Aishwarya Batra] solved usecase-1

--- src/PalindromeCheckerapp.java
+++ src/PalindromeCheckerapp.java
@@ -4,20 +4,6 @@ public class PalindromeCheckerapp {
         System.out.println("version1.0");
         System.out.println("system initialise succesfully");
 
-        String pal="madam";
-        int n=pal.length();
-        String temp="";
-        for(int i=(n-1);i>=0;i--){
-            temp += pal.charAt(i);
-        }
-        if(temp.equals(pal)){
-            System.out.println("yes 'madam' is palindrome ");
-        }
-        else{
-            System.out.println("no 'madam' is not palindrome");
-        }
-
-
 
     }
 }
