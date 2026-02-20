7f406836d7eaf6a5c09d5c2141b81c6a56a1ec16
Aishwarya Batra <aishwaryabatra10@gmail.com>
2026-02-20 09:00:20 +0530

    [Aishwarya Batra] solved usecase-1

---
+++ src/PalindromeCheckerapp.java
@@ -0,0 +1,8 @@
+public class PalindromeCheckerapp {
+    public static void main (String[] args){
+        System.out.println("Welcome to Palindrome checker app management system");
+        System.out.println("version1.0");
+        System.out.println("system initialise succesfully");
+
+    }
+}
