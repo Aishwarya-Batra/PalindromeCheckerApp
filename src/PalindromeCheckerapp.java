f39841239569cb9f8776b966baf30f2c246a730b
Aishwarya Batra <aishwaryabatra10@gmail.com>
2026-02-27 08:40:57 +0530

    [Aishwarya Batra] succesfully completed the usecase-8

--- src/PalindromeCheckerapp.java
+++ src/PalindromeCheckerapp.java
@@ -1,27 +1,98 @@
-import java.util.*;
+class Node {
+    char data;
+    Node next;
+
+    Node(char data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class MyLinkedList {
+    Node head;
+
+    // Add character at end
+    public void addLast(char data) {
+        Node newNode = new Node(data);
+
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = newNode;
+    }
+
+    // Remove first node
+    public char removeFirst() {
+        if (head == null) return '\0';
+
+        char value = head.data;
+        head = head.next;
+        return value;
+    }
+
+    // Remove last node
+    public char removeLast() {
+        if (head == null) return '\0';
+
+        if (head.next == null) {
+            char value = head.data;
+            head = null;
+            return value;
+        }
+
+        Node temp = head;
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+
+        char value = temp.next.data;
+        temp.next = null;
+        return value;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public boolean hasOneElement() {
+        return head != null && head.next == null;
+    }
+}
+
 public class PalindromeCheckerapp {
-    public static void main (String[] args){
-        String word = "madam";
 
-        Deque<Character> deque = new ArrayDeque<>();
+    public static void main(String[] args) {
 
-        for(int i = 0; i < word.length(); i++) {
-            deque.addLast(word.charAt(i));
+        String str = "madam";
+        MyLinkedList list = new MyLinkedList();
+
+        // Add characters to linked list
+        for (int i = 0; i < str.length(); i++) {
+            list.addLast(str.charAt(i));
         }
 
         boolean isPalindrome = true;
 
-        while(deque.size() > 1) {
-            if(deque.removeFirst() != deque.removeLast()) {
+        while (!list.isEmpty() && !list.hasOneElement()) {
+            char first = list.removeFirst();
+            char last = list.removeLast();
+
+            if (first != last) {
                 isPalindrome = false;
                 break;
             }
         }
 
-        if(isPalindrome) {
-            System.out.println(word + " is a palindrome.");
-        } else {
-            System.out.println(word + " is not a palindrome.");
-        }
+        if (isPalindrome)
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
     }
 }
